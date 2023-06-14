2152e5f229f09b24c726af3b473babba597ec0ec
JohnDavidSmith <128341208+JohnDavidSmith@users.noreply.github.com>
2023-06-14 13:22:25 -0400

    Added the Books (one) and the Genre (many) and the Author (many) model relationship in index.js file

--- models/index.js
+++ models/index.js
@@ -1,6 +1,23 @@
 const Books = require("./Books");
 const User = require("./User");
+const Author = require("./Author");
+const Genre = require("./Genre");
 
-//relationships done here
+// Establishing the relationships
+Books.belongsTo(Author, {
+  foreignKey: "author_id",
+});
 
-module.exports = { User, Books };
+Author.hasMany(Books, {
+  foreignKey: "author_id",
+});
+
+Books.belongsTo(Genre, {
+  foreignKey: "genre_id",
+});
+
+Genre.hasMany(Books, {
+  foreignKey: "genre_id",
+});
+
+module.exports = { User, Books, Author, Genre };
