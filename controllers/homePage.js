e4c8e951874fef66819604f6b90b3e9302d1b2e9
Albikras <124819029+Albikras@users.noreply.github.com>
2023-06-14 14:32:30 -0400

    added hompage.js to public

--- controllers/homePage.js
+++ controllers/homePage.js
@@ -1,14 +1,11 @@
 const router = require("express").Router();
-const withAuth = require("../utils/auth.js");
 
-router.get("/", withAuth, async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    res.render("homepage");
+    res.render("homepage", { title: "Explore" });
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-//how do i let them just view the homepage
-//tell them they need to login before going any further but still lets them view the homepage
-//do I need the api information in here?
+module.exports = router;
