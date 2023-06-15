ec4a9b85dcac30c1ad2bfd87f9632f9d21d9c909
Griffin Pelletier <umbral.lights@live.com>
2023-06-15 12:40:08 -0400

    Added initial seeds for sql

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,10 +1,19 @@
 const sequelize = require("../config/connection");
+const { Books } = require("../models");
 const { User } = require("../models");
+const { bookFunction } = require("./bookSeed");
 
 const userData = require("./postData.json");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
+
+  try {
+    await Books.bulkCreate(bookData);
+    console.log("Books seeded successfully");
+  } catch (err) {
+    console.error(err);
+  }
 
   await User.bulkCreate(userData, {
     individualHooks: true,
